061dd4e81a6a74da4e1b087397bb1ff4eb993bb6
tiagohoch <tiagohoch@gmail.com>
2014-11-10 15:12:19 -0200

    consolidating

---
+++ src/android/Caller.java
@@ -0,0 +1,82 @@
+package com.hochkraft.plugins.privilegedcall;
+
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.PluginResult;
+import org.apache.cordova.PluginResult.Status;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+
+/**
+ * This class echoes a string called from JavaScript.
+ */
+//Changed from CDNsms to sms
+public class Caller extends CordovaPlugin {
+    private static final String SMS_GENERAL_ERROR = "SMS_GENERAL_ERROR";
+    private static final String NO_SMS_SERVICE_AVAILABLE = "NO_SMS_SERVICE_AVAILABLE";
+    private static final String SMS_FEATURE_NOT_SUPPORTED = "SMS_FEATURE_NOT_SUPPORTED";
+    private static final String SENDING_SMS_ID = "SENDING_SMS";
+
+    @Override
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        if (action.equals("sendMessage")) {
+            String phoneNumber = args.getString(0);
+            //String message = args.getString(1);
+            
+            boolean isSupported = getActivity().getPackageManager().hasSystemFeature(PackageManager.FEATURE_TELEPHONY);
+            
+            if (! isSupported) {
+                JSONObject errorObject = new JSONObject();
+                
+                errorObject.put("code", SMS_FEATURE_NOT_SUPPORTED);
+                errorObject.put("message", "Calls are not supported on this device");
+                
+                callbackContext.sendPluginResult(new PluginResult(Status.ERROR, errorObject));
+                return false;
+            }
+            
+            this.makeCall(phoneNumber, callbackContext);
+            
+            return true;
+        }
+        
+        return false;
+    }
+
+    private void makeCall(String phoneNumber , final CallbackContext callbackContext) throws JSONException {
+    try {
+
+ 		JSONObject arg_object = args.getJSONObject(0);
+
+		if (ACTION_DIAL_NUMBER.equals(action)) { 
+			String toDial = "tel:" + arg_object.getString("number");
+			Intent callIntent = new Intent(Intent.ACTION_CALL, Uri.parse(toDial)); 
+            this.cordova.getActivity().startActivity(callIntent);
+			callbackContext.success();
+			return true;
+		}
+		callbackContext.error("Invalid action");
+		return false;
+		} catch(Exception e) {
+		System.err.println("Exception: " + e.getMessage());
+		callbackContext.error(e.getMessage());
+		return false;
+		} 
+
+	}
+    }
+    
+    private Activity getActivity() {
+        return this.cordova.getActivity();
+    }
+}
---
+++ www/caller.js
@@ -0,0 +1,34 @@
+var callerExport = {};
+
+callerExport.sendMessage = function(messageInfo, successCallback, errorCallback) {
+    if (messageInfo == null || typeof messageInfo !== 'object') {
+    
+        if (errorCallback) {
+            errorCallback({
+                code: "INVALID_INPUT",
+                message: "Invalid Input"
+            });
+        }
+       
+        return;
+    }
+           
+    var phoneNumber = messageInfo.phoneNumber;
+           
+    if (! phoneNumber) {
+        console.log("Missing Phone Number");
+    
+        if (errorCallback) {
+            errorCallback({
+                code: "MISSING_PHONE_NUMBER",
+                message: "Missing Phone number"
+            });
+        }
+           
+        return;
+    }
+           
+    cordova.exec(successCallback, errorCallback, "Caller", "sendMessage", [phoneNumber]);
+};
+
+module.exports = callerExport;
